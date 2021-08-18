c251b5f9225b8dbd398a7235fa91bebab465da07
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-08-18 16:42:33 +0600

    Changes fixed

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+	apiKey: 'AIzaSyBbYopmCVjwYGl__X3_6oSDd7szzvqY-vc',
+	authDomain: 'fresh-valley-a34ef.firebaseapp.com',
+	projectId: 'fresh-valley-a34ef',
+	storageBucket: 'fresh-valley-a34ef.appspot.com',
+	messagingSenderId: '232961059738',
+	appId: '1:232961059738:web:d0355529d69137d193e823',
 };
 
 export default firebaseConfig;
